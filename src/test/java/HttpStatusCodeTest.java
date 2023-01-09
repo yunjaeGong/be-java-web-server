321d899c4988696f71c6254e4fecad6296752eb0
YunjaeGong <llegokid@naver.com>
2023-01-09 18:28:16 +0900

    feat: HTTP StatusCode Enum 추가
    toString 메서드로 Http Response Header 쉽게 생성하기 위해 status code 및 메시지를 필드로 가지는 Enum 추가

---
+++ src/test/java/HttpStatusCodeTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static utility.HttpStatusCode.FOUND;
+
+public class HttpStatusCodeTest {
+    private static final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private static final PrintStream originalOut = System.out;
+
+    @BeforeAll
+    public static void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterAll
+    public static void restoreStreams() {
+        System.setOut(originalOut);
+    }
+    @Test
+    public void Given_ResponseCode_When_Valid_Then_ToString() {
+        System.out.print(FOUND);
+        assertEquals("302 FOUND",outContent.toString());
+    }
+
+}
