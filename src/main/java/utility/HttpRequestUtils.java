3e1518b1c1ef8cb93761c11bdf3e35c553a545df
YunjaeGong <llegokid@naver.com>
2023-01-10 14:06:27 +0900

    feat: HTTP Request-Line 파싱 메서드 추가
    `GET /abc/abcd.html HTTP/1.1` Request Line 파싱, Map으로 반환

--- src/main/java/utility/HttpRequestUtils.java
+++ src/main/java/utility/HttpRequestUtils.java
@@ -1,11 +1,15 @@
 package utility;
 
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class HttpRequestUtils {
+    public static final String METHOD_TYPE = "methodType";
+    public static final String HTTP_VERSION = "httpVersion";
+    public static final String PATH = "path";
+    public static final String QUERY_STRING = "queryString";
     /*
     QueryStringParam 파싱 지원하는 클래스
      */
@@ -22,5 +26,20 @@ public class HttpRequestUtils {
             return null;
 
         return Stream.of(params).map((param) -> param.split("=")).collect(Collectors.toMap(a->a[0], a->a[1]));
+    }
+
+    public static Map<String, String> parseRequestLine(String requestHeader) {
+        Map<String, String> requestResources = new HashMap<>();
+        String[] header = requestHeader.split(" ");
+        String queryPath = header[1];
+
+        requestResources.put(METHOD_TYPE, header[0]);
+        requestResources.put(HTTP_VERSION, header[2]);
+
+        String[] queries = queryPath.split("\\?");
+        requestResources.put(PATH, queries[0]);
+        requestResources.put(QUERY_STRING, queries[1]);
+
+        return requestResources;
     }
 }
