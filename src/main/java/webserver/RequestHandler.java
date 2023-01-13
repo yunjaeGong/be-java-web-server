b6e5dd1b8d961931f38fdfdc0ff4629e1617e93b
YunjaeGong <llegokid@naver.com>
2023-01-13 12:25:03 +0900

    refactor: 의존하지 않는 상수 삭제

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -9,10 +9,6 @@ import org.slf4j.LoggerFactory;
 import dto.HttpResponse;
 
 public class RequestHandler implements Runnable {
-
-    private static final String DEFAULT_PATH = "/Users/rentalhub-mac88/Desktop/Softeer/be-java-web-server/src/main/resources";
-    private static final String TEMPLATES_PATH = "/templates";
-    private static final String STATIC_PATH = "/static";
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
