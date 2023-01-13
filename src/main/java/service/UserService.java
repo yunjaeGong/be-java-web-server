ae535f6f3a86a6cb66b603b00f4e51c5d71b19b5
YunjaeGong <llegokid@naver.com>
2023-01-13 14:53:30 +0900

    feat: UserService 메서드 static method로 변경

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 public class UserService {
     private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
-    public void signUpUser(User user) throws IllegalStateException {
+    public static void signUpUser(User user) throws IllegalStateException {
         Database.findUserById(user.getUserId())
                 .ifPresent(a->{
                     throw new IllegalStateException("중복된 아이디입니다. 다른 아이디를 사용해주세요.");
@@ -20,7 +20,7 @@ public class UserService {
         Database.addUser(user);
     }
 
-    public User findUserById(String userId) {
+    public static User findUserById(String userId) {
         return Database.findUserById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("해당 유저가 존재하지 않습니다."));
     }
