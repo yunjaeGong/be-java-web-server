cdf49f7238f9a7c36b51a9312b1836826d179953
YunjaeGong <llegokid@naver.com>
2023-01-16 14:04:08 +0900

    feat: Http Request의 body 잘 읽어오는지 테스트 추가

--- src/test/java/HttpRequestTest.java
+++ src/test/java/HttpRequestTest.java
@@ -1,15 +1,32 @@
+import db.Database;
+import dto.HttpResponse;
+import model.User;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import service.UserService;
 import utility.HttpRequest;
+import utility.HttpRequestUtils;
+import utility.HttpStatusCode;
 
 import java.io.ByteArrayInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static java.util.Map.entry;
+import static org.junit.jupiter.api.Assertions.*;
+import static utility.HttpRequestUtils.parseQueryString;
 
 public class HttpRequestTest {
 
-    private static final String DEFAULT_PATH = "/Users/rentalhub-mac88/Desktop/Softeer/be-java-web-server/src/main/resources";
+    private static final String DEFAULT_PATH = "./src/main/resources";
+
+    private static final Logger logger = LoggerFactory.getLogger(HttpRequestTest.class);
+
 
     @Test
     public void Given_RootUrl_When_parseRequestUrl_Then_RootUrl() throws IOException {
@@ -20,11 +37,11 @@ public class HttpRequestTest {
                 "Accept: */* \r\n";
 
         // when
-        HttpRequest parser = new HttpRequest(new ByteArrayInputStream(request.getBytes()));
+        HttpRequest parsedObject = new HttpRequest(new ByteArrayInputStream(request.getBytes()));
 
         // then
-        assertNotNull(parser);
-        assertEquals("/", parser.getPath());
+        assertNotNull(parsedObject);
+        assertEquals("/", parsedObject.getPath());
     }
 
     @Test
@@ -36,10 +53,94 @@ public class HttpRequestTest {
                 "Accept: */* \r\n";
 
         // when
-        HttpRequest parser = new HttpRequest(new ByteArrayInputStream(request.getBytes()));
+        HttpRequest parsedObject = new HttpRequest(new ByteArrayInputStream(request.getBytes()));
 
         // then
-        assertNotNull(parser);
-        assertEquals("/index.html", parser.getPath());
+        assertNotNull(parsedObject);
+        assertEquals("/index.html", parsedObject.getPath());
+    }
+
+    // POST
+    @Test
+    public void Given_PostSignUp_When_SignUp_Then_SignUpUser() throws IOException {
+        // given
+        String request = "POST /user/create HTTP/1.1 \r\n" +
+                "Host: localhost:8080 \r\n" +
+                "Connection: keep-alive \r\n" +
+                "Content-Length: 74 \r\n" +
+                "Content-Type: application/x-www-form-urlencoded \r\n" +
+                "Accept: */* \r\n" +
+                "\r\n" +
+                "userId=javajigi&password=password&name=javajigi&email=javajigi%40slipp.net";
+
+        // when
+        HttpRequest parsedRequest = new HttpRequest(new ByteArrayInputStream(request.getBytes()));
+        String body = parsedRequest.getBody();
+
+        Map<String, String> userQuery = HttpRequestUtils.parseQueryString(body);
+
+        User user = User.UserBuilder.builder()
+                .setUserId(userQuery.get("userId"))
+                .setPassword(userQuery.get("password"))
+                .setName(userQuery.get("name"))
+                .setEmail(userQuery.get("email")).build();
+
+        try {
+            UserService.signUpUser(user);
+        } catch (IllegalStateException e) {
+            logger.error("/user/create - " + e.getMessage());
+            entry("Location", "/user/form.html");
+        }
+
+        List<String> expected = new ArrayList<>();
+        expected.add("userId=javajigi&password=password&name=javajigi&email=javajigi%40slipp.net");
+
+        List<String> result = new ArrayList<>();
+        result.add(parsedRequest.getBody());
+        // then
+        assertLinesMatch(result,expected);
+
+        assertTrue(Database.findUserById("javajigi").isPresent());
+    }
+
+    @Test
+    public void Given_PostSignUp_When_SignUp_Then_ResponseWithRedirection() throws IOException {
+        // given
+        String request = "POST /user/create HTTP/1.1 \r\n" +
+                "Host: localhost:8080 \r\n" +
+                "Connection: keep-alive \r\n" +
+                "Content-Length: 74 \r\n" +
+                "Content-Type: application/x-www-form-urlencoded \r\n" +
+                "Accept: */* \r\n" +
+                "\r\n" +
+                "userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net";
+
+        // when
+        HttpRequest parsedRequest = new HttpRequest(new ByteArrayInputStream(request.getBytes()));
+        String body = parsedRequest.getBody();
+
+        Map<String, String> userQuery = HttpRequestUtils.parseQueryString(body);
+
+        User user = User.UserBuilder.builder()
+                .setUserId(userQuery.get("userId"))
+                .setPassword(userQuery.get("password"))
+                .setName(userQuery.get("name"))
+                .setEmail(userQuery.get("email")).build();
+
+        HttpResponse response = new HttpResponse("",
+                HttpStatusCode.FOUND,
+                Map.ofEntries(entry("Location", "/index.html")),
+                "text/html");
+
+        try {
+            UserService.signUpUser(user);
+        } catch (IllegalStateException e) {
+            logger.error("/user/create - " + e.getMessage());
+            entry("Location", "/user/form.html");
+        }
+
+        DataOutputStream outputStream = response.of(new DataOutputStream(System.out));
+
+        outputStream.flush();
     }
 }
