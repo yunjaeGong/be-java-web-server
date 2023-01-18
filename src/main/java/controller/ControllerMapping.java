d6f010a520a2c6f1d078276b10a09db271cd44ae
YunjaeGong <llegokid@naver.com>
2023-01-18 16:33:20 +0900

    feat: 요청의 method type, 자원의 경로에 따라 각 요청 처리할 메서드로의 mapping 위한 Annotation
    reflection, annotation 이용해 각 요청을 처리할 메서드로의 controller mapping 처리

---
+++ src/main/java/controller/ControllerMapping.java
@@ -0,0 +1,16 @@
+package controller;
+
+import utility.HttpMethodType;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ControllerMapping {
+    HttpMethodType method();
+    String path() default "/";
+
+}
