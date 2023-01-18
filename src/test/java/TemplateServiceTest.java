0c87c1ce38efa5b895ab8758953b9df39edfd470
YunjaeGong <llegokid@naver.com>
2023-01-18 14:39:03 +0900

    feat: html tag 찾고, html 요소 삽입하는 메서드 테스트

--- src/test/java/TemplateServiceTest.java
+++ src/test/java/TemplateServiceTest.java
@@ -10,17 +10,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class TemplateServiceTest {
 
     @Test
-    public void Given_StringToReplace_When_replaceWithString_ThenReplace() throws IOException {
-        String body = Files.readString(new File("src/main/resources/templates/index.html").toPath());
-
-        StringBuilder sb = new StringBuilder(body);
-
-
-        TemplateService.replaceWithString(sb, "로그인", "홍길동동동동동동");
-        assertThat(sb.indexOf("홍길동동동동동동")).isNotNegative();
-    }
-
-    @Test
     public void Given_StringToFind_When_findTag_ThenSuccess() throws IOException {
         String body = Files.readString(new File("src/main/resources/templates/index.html").toPath());
 
@@ -44,5 +33,31 @@ public class TemplateServiceTest {
         expectedIdx = sb.indexOf("로그인", expectedIdx);
 
         assertThat(idx).isEqualTo(expectedIdx);
+    }
+
+
+    @Test
+    public void Given_StringToReplace_When_replaceWithString_ThenReplace() throws IOException {
+        String body = Files.readString(new File("src/main/resources/templates/index.html").toPath());
+
+        StringBuilder sb = new StringBuilder(body);
+
+
+        TemplateService.replaceWithString(sb, "로그인", "홍길동동동동동동");
+        assertThat(sb.indexOf("홍길동동동동동동")).isNotNegative();
+    }
+
+    @Test
+    public void Given_StringToReplace_When_replaceStringWithTags_ThenReplace() throws IOException {
+        String body = Files.readString(new File("src/main/resources/templates/user/list.html").toPath());
+
+        StringBuilder sb = new StringBuilder(body);
+        int openingIdx = TemplateService.findTag(sb, "<tbody>", 1);
+        int closingIdx = TemplateService.findTag(sb, "</tbody>", 1);
+        String toReplace = "<tr>\n" +
+                "                    <th scope=\"row\">1</th> <td>javajigi</td> <td>자바지기</td> <td>javajigi@sample.net</td><td><a href=\"#\" class=\"btn btn-success\" role=\"button\">수정</a></td>\n" +
+                "                </tr>";
+        TemplateService.replaceTag(sb, toReplace, openingIdx, closingIdx-openingIdx, "<tbody>");
+        System.out.println(sb);
     }
 }
