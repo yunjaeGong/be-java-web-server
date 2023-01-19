8480482ebd9dde5c998b607558c658774e71a265
YunjaeGong <llegokid@naver.com>
2023-01-19 16:50:47 +0900

    feat: 각 요청의 처리를 수행할 메서드 매핑 수행하는 ControllerManager 클래스 실제 적용

--- src/main/java/controller/ControllerManager.java
+++ src/main/java/controller/ControllerManager.java
@@ -1,22 +1,29 @@
 package controller;
 
-import db.Database;
 import dto.HttpRequest;
+import dto.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import utility.HttpMethodType;
+import utility.HttpStatusCode;
 
 import java.io.*;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.util.Map.entry;
 
 public class ControllerManager {
-
     private static final Logger logger = LoggerFactory.getLogger(ControllerManager.class);
+
+    public static final String DEFAULT_PATH = "./src/main/resources";
+    public static final String TEMPLATES_PATH = "/templates";
+    public static final String STATIC_PATH = "/static";
 
     private static Map<Class<?>, Object> instance;
     private static List<Class<?>> controllers;
@@ -29,31 +36,45 @@ public class ControllerManager {
         controllers = List.of(UserController.class, ViewController.class);
     }
 
-    public static void main(String[] args) throws IOException, InvocationTargetException, IllegalAccessException {
-        String request = "GET /index.html HTTP/1.1 \r\n" +
-                "Host: localhost:8080 \r\n" +
-                "Connection: keep-alive \r\n" +
-                "Accept: */* \r\n";
+    public static HttpResponse matchController(HttpRequest request) throws IOException {
+        StringBuilder resourcePath = new StringBuilder(DEFAULT_PATH);
+        String path = request.getPath();
+        String contentType = Files.probeContentType(Path.of(path));
 
-        // when
-        HttpRequest req = new HttpRequest(new ByteArrayInputStream(request.getBytes()));
+        boolean ifStaticResource = false;
 
         for(Class con : controllers) {
             List<Method> methods = Arrays.stream(con.getMethods()).filter(m -> m.isAnnotationPresent(ControllerMapping.class)).collect(Collectors.toList());
             Method method = null;
-            Object controllerInstance = instance.get(con);
 
-            method = methods.stream().filter((m)->ifMatchController(m, req)).findFirst().orElse(null);
+            method = methods.stream().filter((m) -> ifMatchController(m, request)).findFirst().orElse(null);
 
             if(method == null)
                 continue;
 
             try {
-                method.invoke(controllerInstance, req);  // TODO: Controller 인스턴스 주입
-            } catch (IllegalArgumentException | IllegalAccessException | InvocationTargetException | NullPointerException e) {
+                Object controllerInstance = instance.get(con);
+                return (HttpResponse) method.invoke(controllerInstance, request);
+            } catch (IllegalArgumentException | IllegalAccessException | NullPointerException e) {
                 logger.error(e.getMessage());
+            } catch (InvocationTargetException e) {
+                logger.error(e.getMessage());
+
             }
         }
+
+        if(path.contains("html") || path.contains(".ico")) {
+            resourcePath.append(TEMPLATES_PATH);
+
+        } else {
+            resourcePath.append(STATIC_PATH);
+        }
+        resourcePath.append(path);
+
+        logger.debug("contentType: " +contentType);
+
+        return new HttpResponse(resourcePath.toString(), HttpStatusCode.OK, contentType);
+
     }
 
     private static boolean ifMatchController(Method method, HttpRequest req) {
