6f8df46ee1a463e9ccb8bf296f0a456ab0c9c1cd
YunjaeGong <llegokid@naver.com>
2023-01-26 13:47:39 +0900

    chore: Session의 createDate를 DB에 저장 후 읽어온 값이 쓴 값과 일치하는지 테스트

--- src/test/java/SessionTest.java
+++ src/test/java/SessionTest.java
@@ -1,19 +1,27 @@
+import db.SessionDatabase;
 import db.UserDatabase;
+import dto.Session;
 import dto.SessionCookie;
 import model.User;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import service.SessionService;
+import service.UserService;
 
 import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class SessionTest {
+    private static final Logger logger = LoggerFactory.getLogger(SessionTest.class);
 
     @BeforeEach
     private void clearUserDatabase() {
         UserDatabase.clear();
+        SessionDatabase.clear();
     }
 
     @Test
@@ -30,4 +38,18 @@ public class SessionTest {
         System.out.print(session);
     }
 
+    @Test
+    @DisplayName("Session의 createDate를 DB에 저장 후 읽어온 값이 쓴 값과 일치하는지 테스트")
+    public void createDateTest() {
+        // given
+
+        // when
+        Session expected = SessionService.createSession("gildong", "Hong gildong", Map.of());
+        logger.debug("expected uid: {}", expected.getSessionId());
+
+        // then
+        Session found = SessionService.findSessionBySID(expected.getSessionId());
+        logger.debug("found uid: {}", found.getSessionId());
+        assertThat(found.getCreateDate()).isEqualTo(found.getCreateDate());
+    }
 }
