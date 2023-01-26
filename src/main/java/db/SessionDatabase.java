ff8e239627d47fdd28d4cd97ea8a27e05c951b69
YunjaeGong <llegokid@naver.com>
2023-01-26 16:49:54 +0900

    rollback: 기존 HashMap 기반 Session 저장소로 롤백

--- src/main/java/db/SessionDatabase.java
+++ src/main/java/db/SessionDatabase.java
@@ -1,90 +1,29 @@
 package db;
 
 import dto.Session;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import service.DBConnection;
 
-import java.sql.*;
-import java.util.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 public class SessionDatabase {
-    private static final Logger logger = LoggerFactory.getLogger(SessionDatabase.class);
-    private static final Connection con = DBConnection.getInstance();
+
+    private static Map<String, Session> sessions = new HashMap<>();
 
     public static Optional<Session> findSessionBySid(String sid) {
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        Session session;
-
-        try {
-            String sql = "SELECT * FROM sessions where sid = ?";
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, sid);
-            rs = pstmt.executeQuery();
-
-            if(rs == null || !rs.next())
-                return Optional.empty();
-
-            session = new Session(rs.getString("sid"),
-                    rs.getString("userId"),
-                    rs.getString("userName"),
-                    rs.getTimestamp("createDate").toLocalDateTime());
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return Optional.of(session);
+        return Optional.ofNullable(sessions.get(sid));
     }
 
     public static void addSession(Session s) {
-        PreparedStatement pstmt = null;
-        try {
-            String sql = "INSERT INTO sessions VALUES (?, ?, ?, ?)";
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, s.getSessionId());
-            pstmt.setString(2, s.getUserId());
-            pstmt.setString(3, s.getUserName());
-            pstmt.setTimestamp(4, Timestamp.valueOf(s.getCreateDate()));
-
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        sessions.put(s.getSessionId(), s);
     }
 
     public static Optional<Collection<Session>> findAll() {
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        List<Session> sessions = new ArrayList<>();
-
-        try {
-            String sql = "SELECT * FROM sessions";
-            pstmt = con.prepareStatement(sql);
-            rs = pstmt.executeQuery();
-
-            sessions.add(new Session(rs.getString("sessionId"),
-                    rs.getString("userId"),
-                    rs.getString("userName"),
-                    rs.getTimestamp("createDate").toLocalDateTime())
-            );
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-        return Optional.of(sessions);
+        return Optional.of(sessions.values());
     }
 
     public static void clear() {
-        PreparedStatement pstmt = null;
-
-        try {
-            String sql = "TRUNCATE sessions";
-            pstmt = con.prepareStatement(sql);
-
-            pstmt.execute();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        sessions = new HashMap<>();
     }
 }
